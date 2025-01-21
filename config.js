c0c5c6a0249e678050c8e43ada42daea8d8f016c
BaayMeissa <meissambaye09@gmail.com>
2025-01-21 23:10:50 +0000

    config .js modified

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const PORT = 5555;
+export const PORT = process.env.PORT || 5555;
 
 export const mongoDBURL ="mongodb+srv://meissambaye:BaayMeissa6@cluster0.s9x2k.mongodb.net/books?retryWrites=true&w=majority&appName=Cluster0"
