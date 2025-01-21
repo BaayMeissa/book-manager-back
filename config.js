92766a67d22ff92e7caba1d9483388627c502e92
BaayMeissa <meissambaye09@gmail.com>
2025-01-21 23:34:20 +0000

    second modification

--- config.js
+++ config.js
@@ -1,2 +1,1 @@
-export const PORT = process.env.PORT || 5555;
 export const mongoDBURL = process.env.mongoDBURL;
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import { PORT, mongoDBURL } from './config.js';
-import mongoose from 'mongoose';
-import booksRoute from './routes/booksRoute.js';
-import cors from 'cors';
+import express from "express";
+import { mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+const PORT = process.env.PORT || 5555;
 
 const app = express();
 
@@ -12,18 +13,17 @@ app.use(express.json());
 // Middleware for handling CORS POLICY
 app.use(cors());
 
-
-app.get('/', (request, response) => {
+app.get("/", (request, response) => {
   console.log(request);
-  return response.status(234).send('Welcome To MERN Stack Tutorial');
+  return response.status(234).send("Welcome To MERN Stack Tutorial");
 });
 
-app.use('/books', booksRoute);
+app.use("/books", booksRoute);
 
 mongoose
   .connect(mongoDBURL)
   .then(() => {
-    console.log('App connected to database');
+    console.log("App connected to database");
     app.listen(PORT, () => {
       console.log(`App is listening to port: ${PORT}`);
     });
